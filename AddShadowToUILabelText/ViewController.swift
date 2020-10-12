a0185234b5295bf80e2fbb331650e8f890f255b1
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-10-12 11:31:31 +0300

    init

--- AddShadowToUILabelText/ViewController.swift
+++ AddShadowToUILabelText/ViewController.swift
@@ -11,7 +11,18 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let name = UILabel.init()
+        name.frame = CGRect(x: 10.0, y: 40.0, width: UIScreen.main.bounds.size.width - 20.0, height: 45)
+        name.text = "STAR WARS"
+        name.font = UIFont(name: "verdana", size: 40.0)
+        name.textAlignment = .center
+        name.textColor = .darkGray
+        name.numberOfLines = 0
+        self.view.addSubview(name)
+        
+        name.shadowColor = .lightGray
+        name.shadowOffset = CGSize(width: 3.0, height: 5.0)
     }
 
 
